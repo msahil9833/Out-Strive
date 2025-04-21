83c1168984755892d2b6664aeef1f419ad6f0e29
Mohammad Sahil <86947345+msahil9833@users.noreply.github.com>
2025-04-21 18:18:44 +0530

    assets final

---
+++ assets/img.js
@@ -0,0 +1,161 @@
+import Logo from "./images/logo.svg";
+import HomeBannerDot from "./images/home-banner-dot.png";
+import Workato from "./images/workato.png";
+import Exabeam from "./images/exabeam.png";
+import Airwallex from "./images/airwallex.png";
+import Netskope from "./images/netskope.png";
+import Attentive from "./images/attentive.png";
+import FooterTop from "./images/footer-top.png";
+import FooterTopBg from "./images/footer-top-bg.png";
+import FooterLogo from "./images/footer-logo.png";
+import HomeBannerBg from "./images/home-banner-bg.png";
+import GridBg from "./images/grid-bg.png";
+import HomeSmartEppMain from "./images/home-smart-epp-main.png";
+import HomeSmartEppDot from "./images/home-smart-epp-dot.png";
+import HomeSmartEppBottomLeft from "./images/home-smart-epp-bottom-left.png";
+import VideoThumbnail from "./images/video-thumbnail.png";
+import AboutBanner from "./images/about-banner.png";
+import AboutDb from "./images/about-db.svg";
+import ContactBanner from "./images/contact-banner.png";
+import ContactForm from "./images/contact-form.png";
+import TeamMember1 from "./images/team-member1.png";
+import Partner1 from "./images/partner1.png";
+import HrmsBannerIcon from "./images/hrms-banner-icon.png";
+import HrmsBanner from "./images/hrms-banner.png";
+import HrmsProposal1 from "./images/hrms-proposal1.png";
+import HrmsProposalIcon from "./images/hrms-proposal-icon.png";
+import ItmsBanner from "./images/itms-banner.png";
+import WithItmsHeading from "./images/with-itms-heading.png";
+import WithItmsMainImg from "./images/with-itms-main-img.png";
+import ibm from "./images/ibm.png";
+import paloalto from "./images/paloalto.png";
+import brilyant from "./images/brilyant.png";
+import HRMGenie from "./images/HRMGenie.png";
+import TATGenie from "./images/TATGenie.png";
+import AssetGenie from "./images/AssetGenie.png";
+import EPPGenie from "./images/EPPGenie.png";
+import HRMSHomepage from "./images/HRMSHomepage.png";
+import HRMSTable from "./images/HRMSTable.png";
+import HRMSTabletwo from "./images/HRMSTabletwo.png";
+import HRMSTablethree from "./images/HRMSTablethree.png";
+
+import hrmstwo from "./images/hrmstwo.png";
+import hrmsthree from "./images/hrmsthree.png";
+import hrmsfour from "./images/hrmsfour.png";
+import eppguine from "./images/eppguine.png";
+import epptwo from "./images/epptwo.png";
+import eppthree from "./images/eppthree.png";
+import eppfour from "./images/eppfour.jpg";
+import ITSMHomepage from "./images/ITSMHomepage.png";
+import ITSMtwo from "./images/ITSMtwo.png";
+import itam from "./images/itam.png";
+import itamtwo from "./images/itamtwo.png";
+import footerlogo from "./images/footerlogo.png";
+import ticketicon from "./images/ticketicon.png";
+import changeicon from "./images/changeicon.png";
+import selficon from "./images/selficon.png";
+import managmenticon from "./images/managmenticon.png";
+import webicon from "./images/webicon.png";
+import partyicon from "./images/partyicon.png";
+import iticon from "./images/iticon.png";
+import workicon from "./images/workicon.png";
+import automationicon from "./images/automationicon.png";
+import efficiencyicon from "./images/efficiencyicon.png";
+import integrationicon from "./images/integrationicon.png";
+import proactiveicon from "./images/proactiveicon.png";
+import checkicon from "./images/checkicon.png";
+import developmenticon from "./images/developmenticon.png";
+import wheelicon from "./images/wheelicon.png";
+import edgeicon from "./images/edgeicon.png";
+import upicon from "./images/upicon.png";
+import timeicon from "./images/timeicon.png";
+import boyicon from "./images/boyicon.png";
+import titlehrm from "./images/titlehrm.png";
+import titletat from "./images/titletat.png";
+import titleasset from "./images/titleasset.png";
+import titleepp from "./images/titleepp.png";
+import ContactMailIcon from "./images/contact-mail-icon.png";
+import ContactPhoneIcon from "./images/contact-phone-icon.svg";
+export const IMG = {
+  titleasset,
+  titleepp,
+  titletat,
+  titlehrm,
+  edgeicon,
+  upicon,
+  timeicon,
+  boyicon,
+  wheelicon,
+  developmenticon,
+  checkicon,
+  automationicon,
+  efficiencyicon,
+  integrationicon,
+  proactiveicon,
+
+  webicon,
+  partyicon,
+  iticon,
+  workicon,
+  managmenticon,
+  selficon,
+  changeicon,
+  ticketicon,
+  footerlogo,
+  itamtwo,
+  itam,
+  ITSMtwo,
+  ITSMHomepage,
+
+  epptwo,
+  eppthree,
+  eppfour,
+
+  eppguine,
+  hrmsfour,
+  hrmstwo,
+  hrmsthree,
+  HRMSTable,
+  HRMSTabletwo,
+  HRMSTablethree,
+
+  HRMSHomepage,
+  EPPGenie,
+  AssetGenie,
+  TATGenie,
+  HRMGenie,
+  ibm,
+  paloalto,
+  brilyant,
+  Logo,
+  HomeBannerDot,
+  Workato,
+  Exabeam,
+  Airwallex,
+  Netskope,
+  Attentive,
+  FooterTop,
+  FooterTopBg,
+  FooterLogo,
+  HomeBannerBg,
+  GridBg,
+  HomeSmartEppMain,
+  HomeSmartEppDot,
+  HomeSmartEppBottomLeft,
+  VideoThumbnail,
+  AboutBanner,
+  AboutDb,
+  ContactBanner,
+  ContactForm,
+  TeamMember1,
+  Partner1,
+  HrmsBannerIcon,
+  HrmsBanner,
+  HrmsProposal1,
+  HrmsProposalIcon,
+  ItmsBanner,
+  WithItmsHeading,
+  WithItmsMainImg,
+  ContactMailIcon,
+  ContactPhoneIcon,
+};
